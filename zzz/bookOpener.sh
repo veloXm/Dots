f895cea81b26025fd4d3011cfcf4ada6e207c74a
Mitesh Soni <smiteshhc@gmail.com>
2024-07-29 13:43:01 +0530

    Fixed typo

--- zzz/bookOpener.sh
+++ zzz/bookOpener.sh
@@ -11,8 +11,8 @@ SELECTED_FILE=$(echo "$FILES" | rofi -dmenu -p "Select a file to open:")
 
 # If file was selected then open it otherwise print error
 if [[ -n "$SELECTED_FILE" ]]; then
-    xdg-open "$BOOKS_DIR/$SELECTED_FILE"
     dunstify "Opening $SELECTED_FILE"
+    xdg-open "$BOOKS_DIR/$SELECTED_FILE"
 else
     dunstify "No file selected"
 fi
