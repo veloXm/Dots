9e0492eb542de71dfa01f7cf72fd63fbded30fe2
Mitesh Soni <smiteshhc@gmail.com>
2024-07-31 18:40:03 +0530

    Updated bookOpener

--- zzz/bookOpener.sh
+++ zzz/bookOpener.sh
@@ -11,8 +11,14 @@ SELECTED_FILE=$(echo "$FILES" | rofi -dmenu -p "Select a file to open:")
 
 # If file was selected then open it otherwise print error
 if [[ -n "$SELECTED_FILE" ]]; then
-    dunstify "Opening $SELECTED_FILE"
+    dunstify "Opening $SELECTED_FILE..."
     xdg-open "$BOOKS_DIR/$SELECTED_FILE"
+    
+    # Check if opened with success
+    if [[ $? != 0 ]]; then
+        dunstctl close
+        dunstify "An error has occurred."
+    fi
 else
-    dunstify "No file selected"
+    dunstify "No file selected."
 fi
